bf153f2fcbe06712a6cad774d575534bb2bf3142
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-06-28 19:07:54 +0300

    Modified water, create player, download joystick sprites

---
+++ Assets/Sushi_Driver/Scripts/JoystickView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputController))]
+public class JoystickView : MonoBehaviour
+{
+    [SerializeField] private GameObject JoystickObject;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
